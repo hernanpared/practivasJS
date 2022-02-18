69c49de38dcd9673be7b11e4205bd239d3299999
hernanpared <hernan2014@hotmail.com>
2022-02-18 11:14:26 -0300

    final

--- operador ternario.js
+++ operador ternario.js
@@ -1,9 +1,23 @@
-let a = 3;
+let a = 2;
 let b = 4;
 let c = 5;
 
 //operador ternario
+/*sintaxis:
 
-//(condicion) ? true : false
+(condicion) ? true : false
 
-a % 2 === 0 ? console.log(`${a} es par`) : console.log(`$(a) es impar`);
+evalua una condicion , la misma debe ser true o false
+
+*/
+
+//a % 2 === 0 ? console.log(`${a} es par`) : console.log(`$(a) es impar`);
+
+//en el caso de tener varias sentencias se trabaja de la siguiente forma:
+
+a % 2 === 0 ?
+ (
+     console.log(`${a} es par`), console.log(`${a} divisible 2`)
+     )
+  : 
+  console.log(`${a} es impar`);
